f76679e1c01d45c114ad6893857181c594f4abc6
Laurecia <61845061+Laurecia@users.noreply.github.com>
2022-02-07 15:01:41 +0400

    backend files
    fichiers htdocs du serveurs xampp

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+require 'db.php';
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+ 
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)){
+    $request = json_decode($postdata);
+     
+     
+    $UserEmail = $request->UserEmail;
+    $UserPW = $request->UserPW;
+
+    $sql = "INSERT INTO newuser(UserEmail, UserPW) VALUES('$UserEmail', '$UserPW')";
+    if(mysqli_query($db,$sql)){
+        http_response_code(201);
+    }
+    else{
+         http_response_code(422); 
+    }
+         
+}
